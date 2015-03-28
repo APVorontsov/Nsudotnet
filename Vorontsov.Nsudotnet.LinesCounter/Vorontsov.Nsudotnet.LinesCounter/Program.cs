617c5f10100644174dcb6461a49c8baa9076873d
Александр Воронцов <ap.vorontsov@yandex.ru>
2015-03-28 18:10:36 +0600

    Lines Counter
    Lines Counter appears to be done, though the program still may need some
    additional testing.
    You cat set up file filter and custom commentary strings via command
    promt arguments or leave it blank for default values.
    Unfortunately, interactions with quotation marks are completely ignored.

---
+++ Vorontsov.Nsudotnet.LinesCounter/Vorontsov.Nsudotnet.LinesCounter/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vorontsov.Nsudotnet.LinesCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filter, commentsOn, commentsOff, inlineComment;
+            
+            if (args.Length > 0)
+                filter = args[0];
+            else
+                filter = "*.cs";
+
+            if (args.Length > 1)
+                commentsOn = args[1];
+            else
+                commentsOn = "/*";
+
+            if(args.Length > 2)
+                commentsOff = args[2];
+            else
+                commentsOff = "*/";
+
+            if (args.Length > 3)
+                inlineComment = args[3];
+            else
+                inlineComment = "//";
+
+            var files = from file in Directory.EnumerateFiles(Directory.GetCurrentDirectory(), filter, SearchOption.AllDirectories)
+                        select file;
+            int countOfLines = 0;
+            bool readingComments;
+
+            foreach (var file in files)
+            {
+                using (StreamReader sr = File.OpenText(file))
+                {
+                    string l;
+                    readingComments = false;
+
+                    while ((l = sr.ReadLine()) != null)
+                    {
+                        if (l == "" || l.StartsWith(inlineComment))
+                            continue;
+
+                        int i = 0;
+                        bool lineIsNotEmpty = false;
+                        while(i < l.Length && i != -1)
+                        {
+                            int pos;
+                            if (readingComments == false)
+                            {
+                                pos = l.IndexOf(commentsOn, i);
+
+                                if (pos != -1)
+                                    readingComments = true;
+ 
+                                if (pos != i)
+                                    lineIsNotEmpty = true;
+
+                                i = (pos == -1) ? pos : pos + commentsOn.Length;
+                            }
+                            else
+                            {
+                                pos = l.IndexOf(commentsOff, i);
+                                
+                                if (pos != -1)
+                                    readingComments = false;
+
+                                i = (pos == -1) ? pos : pos + commentsOff.Length;
+                            }
+                        }
+
+                        if (lineIsNotEmpty)
+                        {
+                            ++countOfLines;
+                            Console.WriteLine(l);
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(countOfLines);
+            Console.ReadLine();
+/*            foreach (var s in files)
+//                Console.WriteLine(s);
+*/
+        }
+    }
+}
